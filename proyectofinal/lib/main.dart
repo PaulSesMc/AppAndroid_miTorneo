f3755b588563862c8ae04501a797127d53d29c4e
PaulSesMc <paul.sesmc@hotmail.com>
2022-11-29 10:48:18 -0600

    jugadores

--- proyectofinal/lib/main.dart
+++ proyectofinal/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'paginas.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(Paginas());
