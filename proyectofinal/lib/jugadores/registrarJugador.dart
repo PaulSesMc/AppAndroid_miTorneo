f473c0c57d9f3b16ddd5489776185c395cd94cd7
Rodrigo Narvaez <rodrigo_narvaez@outlook.com>
2022-11-30 21:57:16 -0600

    registrarJuagdor este si v100 final dos

--- proyectofinal/lib/jugadores/registrarJugador.dart
+++ proyectofinal/lib/jugadores/registrarJugador.dart
@@ -32,7 +32,8 @@ class registrarJugador extends StatelessWidget{
               //replace with our own icon data.
             )),
         body: //const NuevoJugador(item),
-          Center(
+          SingleChildScrollView(
+                  child: Center(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
@@ -67,6 +68,7 @@ class registrarJugador extends StatelessWidget{
                             decoration: const InputDecoration(
                               hintText: 'Número de jugador',
                             ),
+                            keyboardType: TextInputType.number,
                             validator: (String? value) {
                               if (value == null || value.isEmpty) {
                                 return 'LLenar campo';
@@ -80,6 +82,7 @@ class registrarJugador extends StatelessWidget{
                             decoration: const InputDecoration(
                               hintText: 'Edad del jugador',
                             ),
+                              keyboardType: TextInputType.number,
                             validator: (String? value) {
                               if (value == null || value.isEmpty) {
                                 return 'LLenar campo';
@@ -109,131 +112,19 @@ class registrarJugador extends StatelessWidget{
                           ),
                         ]),
                   ),
-                )
+                ),
+                
 
               ]
             ),
           )
       ),
+                ),
     );
   }
 }
 
 
-/*class NuevoJugador extends StatefulWidget {
-  const NuevoJugador(Team item, {super.key});
-  
-  @override
-  State<NuevoJugador> createState() => _NuevoJugadorState(item);
-}
-
-class _NuevoJugadorState extends State<NuevoJugador> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String nombre = '';
-  String numero = '';
-  String edad = '';
-  
-
-
-  @override
-  Widget build(BuildContext context)  {
-    return Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-      Form(
-        key: _formKey,
-        child: Container(
-          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Image.network(
-                              this.item.logo,
-                              height: 150,
-                              width: 150,
-                            ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Nombre del jugador',
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'LLenar campo';
-                    } else {
-                      nombre = value;
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Numero del jugador',
-                  ),
-                  keyboardType: TextInputType.number,
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'LLenar campo';
-                    } else {
-                      numero = value;
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Edad',
-                  ),
-                  keyboardType: TextInputType.number,
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'LLenar campo';
-                    } else {
-                      edad = value;
-                    }
-                    return null;
-                  },
-                ),
-             /*DropdownButton<String>(
-              value: dropdownValue,
-              icon: const Icon(Icons.arrow_downward),
-              elevation: 16,
-              style: const TextStyle(color: Colors.deepPurple),
-              underline: Container(
-                height: 2,
-                color: Colors.deepPurpleAccent,
-              ),
-              onChanged: (String? value) {
-                // This is called when the user selects an item.
-                setState(() {
-                  dropdownValue = value!;
-                });
-              },
-              items: list),*/
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Validate will return true if the form is valid, or false if
-                      // the form is invalid.
-                      if (_formKey.currentState!.validate()) {
-                        
-                        //print(numero);
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => Paginas()),
-                          (Route<dynamic> route) => false,
-                        );
-                      } else {}
-                    },
-                    child: const Text('Submit'),
-                  ),
-                ),
-              ]),
-        ),
-      )
-    ]));
-  }
-}
-*/
  Future<List<String>> _getEquipos() async{
   final Future<List<Team>> products = fetchTeam();
   print('entra');
